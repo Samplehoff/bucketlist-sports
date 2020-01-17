4bed9e71a20e751a0f216b434bcfeac394bc1704
Joshua Cannedy <jdcannedy@gmail.com>
2020-01-16 19:10:12 -0600

    pushing the navbar login

--- index.js
+++ index.js
@@ -6,110 +6,125 @@ const session = require("express-session");
 require('dotenv').config();
 
 
-var pbkdf2 = require('pbkdf2');
-var salt = process.env.SALT_KEY;
+var mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/public');
 
-function encryptionPassword(password){
-  var key = pbkdf2.pbkdf2Sync(
-    password, salt, 36000, 256, 'sha256'
-  );
-  var hash = key.toString('hex');
-  return hash;
-}
+app.get('/', function(req, res){
+  res.render('index.mustache');
+});
+
+app.get('/bucketlist', function(req, res){
+  res.render('bucketlist.mustache');
+});
+
+app.get('/proffesional', function(req, res){
+  res.render('proffesional.mustache');
+});
+
+// var pbkdf2 = require('pbkdf2');
+// var salt = process.env.SALT_KEY;
+
+// function encryptionPassword(password){
+//   var key = pbkdf2.pbkdf2Sync(
+//     password, salt, 36000, 256, 'sha256'
+//   );
+//   var hash = key.toString('hex');
+//   return hash;
+// }
 app.use(session({secret: "dogs", resave: false, saveUninitialized: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
 
-/*  PASSPORT SETUP  */
+// /*  PASSPORT SETUP  */
 
-const passport = require('passport');
-app.use(passport.initialize());
-app.use(passport.session());
+// const passport = require('passport');
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.get('/success', function(req, res){ 
-  if (req.isAuthenticated()) {
-    res.send("Welcome " + req.query.username + "!!")
-  } else {
-    res.send("not authorized.");
-  }
-  });
+// app.get('/success', function(req, res){ 
+//   if (req.isAuthenticated()) {
+//     res.send("Welcome " + req.query.username + "!!")
+//   } else {
+//     res.send("not authorized.");
+//   }
+//   });
 
-//   app.get('/create_artist', function(req, res){ 
-//     if (req.isAuthenticated()) {
-//       res.redirect('create_artist');
-//     } else {
-//       res.send("not authorized.");
-//     }
+// //   app.get('/create_artist', function(req, res){ 
+// //     if (req.isAuthenticated()) {
+// //       res.redirect('create_artist');
+// //     } else {
+// //       res.send("not authorized.");
+// //     }
+// //     });
+
+
+// app.get('/logout', function(req, res){
+//   if(req.isAuthenticated()) {
+//     console.log("user logged out");
+//     req.logOut();
+//     res.send("user logged out");
+//   } else {
+//     res.send("You don't have a session open")
+//   }
+// });
+
+// app.get('/error', (req, res) => res.send("error logging in"));
+
+// passport.serializeUser(function (user, cb) {
+//   cb(null, user.id);
+// });
+
+// passport.deserializeUser(function (id, cb) {
+//   models.user.findOne({ where: { id: id } }).then(function (user) {
+//     cb(null, user);
+//   });
+// });
+
+// /* PASSPORT LOCAL AUTHENTICATION */
+
+// const LocalStrategy = require('passport-local').Strategy;
+
+// passport.use(new LocalStrategy(
+//   function (username, password, done) {
+//     models.user.findOne({
+//       where: {
+//         username: username
+//       }
+//     }).then(function (user) {
+//       if (!user) {
+//         return done(null, false);
+//       }
+
+//       if (user.password != encryptionPassword(password)) {
+//         return done(null, false);
+//       }
+//       return done(null, user);
+//     }).catch(function (err) {
+//       return done(err);
 //     });
+//   }
+// ));
 
+// app.post('/',
+//   passport.authenticate('local', { failureRedirect: '/error' }),
+//   function(req, res) {
+//     res.redirect('/success');
 
-app.get('/logout', function(req, res){
-  if(req.isAuthenticated()) {
-    console.log("user logged out");
-    req.logOut();
-    res.send("user logged out");
-  } else {
-    res.send("You don't have a session open")
-  }
-});
-
-app.get('/error', (req, res) => res.send("error logging in"));
-
-passport.serializeUser(function (user, cb) {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function (id, cb) {
-  models.user.findOne({ where: { id: id } }).then(function (user) {
-    cb(null, user);
-  });
-});
-
-/* PASSPORT LOCAL AUTHENTICATION */
-
-const LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    models.user.findOne({
-      where: {
-        username: username
-      }
-    }).then(function (user) {
-      if (!user) {
-        return done(null, false);
-      }
-
-      if (user.password != encryptionPassword(password)) {
-        return done(null, false);
-      }
-      return done(null, user);
-    }).catch(function (err) {
-      return done(err);
-    });
-  }
-));
-
-app.post('/',
-  passport.authenticate('local', { failureRedirect: '/error' }),
-  function(req, res) {
-    res.redirect('/success');
-
-  });
+//   });
 
 
 
-app.post("/sign-up", function (req, response) {
-  models.user.create({ username: req.body.username, password: encryptionPassword(req.body.password)})
-    .then(function (user) {
-      response.redirect('/');
+// app.post("/sign-up", function (req, response) {
+//   models.user.create({ username: req.body.username, password: encryptionPassword(req.body.password)})
+//     .then(function (user) {
+//       response.redirect('/');
       
-    });
-});
-
-
+//     });
+// });
 
 
 
