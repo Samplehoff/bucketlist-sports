760dd0f6283ace30e478d45f112bdfcf6ead9ba7
Sampey Hoffman <53267153+Samplehoff@users.noreply.github.com>
2020-02-15 10:59:57 -0600

    google oauth

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     google: {
         clientID: "397615741884-pp50anufr9e0bp03u3bib62lgooq8cd2.apps.googleusercontent.com",
-        clientSecret: "d3wUBQae0RHXMV7F_O_Day"
+        clientSecret: "d3wUBQae0RHXMV7F_O_Day5-"
     },
     session:{
         cookieKey: 'noonecanguessthekey'
